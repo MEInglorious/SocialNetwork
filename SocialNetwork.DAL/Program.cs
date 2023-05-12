e6db495988bf5937e081b83f27299aaf5fb5c76f
Михаил Семенович <semenovich_misha@mail.ru>
2023-05-12 22:26:14 +0300

    Добавьте файлы проекта.

---
+++ SocialNetwork.DAL/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.DAL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для корректной работы приложения запускайте, пожалуйста, Program в PLL.");
+        }
+    }
+}
---
+++ SocialNetwork.PLL/Program.cs
@@ -0,0 +1,53 @@
+﻿using SocialNetwork.BLL.Services;
+using SocialNetwork.PLL.Views;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.PLL
+{
+    class Program
+    {
+        static MessageService messageService;
+        static UserService userService;
+        static FriendService friendService;
+        public static MainView mainView;
+        public static RegistrationView registrationView;
+        public static AuthenticationView authenticationView;
+        public static UserMenuView userMenuView;
+        public static UserInfoView userInfoView;
+        public static UserDataUpdateView userDataUpdateView;
+        public static MessageSendingView messageSendingView;
+        public static UserIncomingMessageView userIncomingMessageView;
+        public static UserOutcomingMessageView userOutcomingMessageView;
+        public static FriendRequestView friendRequestView;
+        public static FriendsView friendsView;
+
+        static void Main(string[] args)
+        {
+            userService = new UserService();
+            messageService = new MessageService();
+            friendService = new FriendService();
+
+            mainView = new MainView();
+            registrationView = new RegistrationView(userService);
+            authenticationView = new AuthenticationView(userService);
+            userMenuView = new UserMenuView(userService);
+            userInfoView = new UserInfoView();
+            userDataUpdateView = new UserDataUpdateView(userService);
+            messageSendingView = new MessageSendingView(messageService, userService);
+            userIncomingMessageView = new UserIncomingMessageView();
+            userOutcomingMessageView = new UserOutcomingMessageView();
+            friendRequestView = new FriendRequestView(friendService, userService);
+            friendsView = new FriendsView();
+
+            while (true)
+            {
+                mainView.Show();
+            }
+        }
+    }
+}
